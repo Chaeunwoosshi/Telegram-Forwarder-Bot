0b29487cb03bc39953075b47e1fa0c2a6996c128
Chaeunwoosshi <engrmentawan@gmail.com>
2025-02-26 23:51:31 +0800

    Update Bot.py

--- source/core/Bot.py
+++ source/core/Bot.py
@@ -2,16 +2,17 @@ from source.menu.AccountSelector import AccountSelector
 from source.menu.MainMenu import MainMenu
 
 class Bot:
-    def __init__(self):
-        self.account_selector = AccountSelector()
+    def __init__(self, api_id, api_hash, bot_token):
+        # Pass the credentials to the AccountSelector
+        self.account_selector = AccountSelector(api_id, api_hash, bot_token)
         self.main_menu = None
 
     async def start(self):
         try:
+            # Select the account with the provided credentials
             telegram = await self.account_selector.select_account()
             self.main_menu = MainMenu(telegram)
             await self.main_menu.start()
         except Exception as err:
             raise err
-
 
